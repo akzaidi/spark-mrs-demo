9a0a56483ceb8372e2d242805167091484b74b7a
Ali Zaidi <alizaidi@microsoft.com>
2016-06-07 18:27:35 +0000

    add nyctaxi test

---
+++ nyctaxitest.R
@@ -0,0 +1,24 @@
+sparkEnvir <- list(spark.executor.instance = '10',
+                   spark.yarn.executor.memoryOverhead = '8000')
+
+sc <- sparkR.init(
+  sparkEnvir = sparkEnvir,
+  sparkPackages = "com.databricks:spark-csv_2.10:1.3.0"
+)
+
+sqlContext <- sparkRSQL.init(sc)
+
+
+dataframe_import <- function(path = "wasb://nyctaxidata@alizaidi.blob.core.windows.net/") {
+  
+  library(SparkR)
+  path <- file.path(path)
+  path_df <- read.df(sqlContext, path,
+                     source = "com.databricks.spark.csv",
+                     header = "true", inferSchema = "true", delimiter = ",")
+  
+  return(path_df)
+  
+}
+
+# full_taxi <- dataframe_import()
